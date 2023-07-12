e2a6322c928457d4143674d087c120be8d17e5cb
CharlotteSP <csmithpe@uwo.ca>
2023-07-12 18:12:49 +0200

    Added setup file, init file, etc.

---
+++ fits_project/fits_package.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+
+
+def open_fits(filename):
+    fits_image_filename = fits.util.get_testdata_filepath(filename)
+    hdul = fits.open(fits_image_filename)
+
+    ### print a summary of the file contents
+    print('\n\nINFO ABOUT FITS FILE:')
+    print('---------------------')
+    hdul.info()
+    return
+
+open_fits('test0.fits')
