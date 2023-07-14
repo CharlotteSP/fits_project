66a8a3de19aed51db3b93a6794ca0e6f828a64f3
CharlotteSP <csmithpe@uwo.ca>
2023-07-14 16:35:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
 
-    name="fits_project",
+    name="fits_project_v2",
 
     version="1.0",
 
     packages=find_packages()
 
 )
-
-entry_points={
-    "console_scripts": [
-        "cmdfits = cmdfits.cli:main",
-    ]
-}
