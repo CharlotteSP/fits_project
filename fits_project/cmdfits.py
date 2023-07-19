49ad9ef986117ea3ee8589d92395e3914e0c725b
Sarah Blunt <sarah.blunt.3@gmail.com>
2023-07-18 18:12:59 -0700

    got cli working

--- fits_project/cmdfits.py
+++ fits_project/cmdfits.py
@@ -7,9 +7,10 @@ from matplotlib.widgets import Button
 from astropy.io import fits
 from tabulate import tabulate
 
+
 class FITSViewer:
-    """Class to display a broswer for FITS files
-    """
+    """Class to display a broswer for FITS files"""
+
     def __init__(self, fits_files):
         """Class Constructor
 
@@ -23,16 +24,16 @@ class FITSViewer:
 
         # Create the figure and axis
         self.fig, self.ax = plt.subplots()
-        self.image_plot = self.ax.imshow(self.image_data, cmap='gray')
+        self.image_plot = self.ax.imshow(self.image_data, cmap="gray")
         self.ax.set_title(self.fits_files[self.current_index])
 
         # Create buttons and connect them to the navigation functions
         self.next_button_ax = plt.axes([0.7, 0.03, 0.1, 0.04])
-        self.next_button = Button(self.next_button_ax, 'Next')
+        self.next_button = Button(self.next_button_ax, "Next")
         self.next_button.on_clicked(self.next_image)
 
         self.prev_button_ax = plt.axes([0.81, 0.03, 0.1, 0.04])
-        self.prev_button = Button(self.prev_button_ax, 'Previous')
+        self.prev_button = Button(self.prev_button_ax, "Previous")
         self.prev_button.on_clicked(self.prev_image)
 
     def next_image(self, event):
@@ -63,6 +64,7 @@ class FITSViewer:
             self.ax.set_title(self.fits_files[self.current_index])
             plt.draw()
 
+
 def display_header_info(fits_files, attributes):
     """Displays header info about the FITS file.
 
@@ -78,19 +80,19 @@ def display_header_info(fits_files, attributes):
                 row = [fits_file]
                 for attribute in attributes:
                     if attribute == "date":
-                        row.append(header.get('DATE-OBS', 'N/A'))
+                        row.append(header.get("DATE-OBS", "N/A"))
                     elif attribute == "time":
-                        row.append(header.get('TIME-OBS', 'N/A'))
+                        row.append(header.get("TIME-OBS", "N/A"))
                     elif attribute == "latitude":
-                        row.append(header.get('LATITUDE', 'N/A'))
+                        row.append(header.get("LATITUDE", "N/A"))
                     elif attribute == "longitude":
-                        row.append(header.get('LONGITUD', 'N/A'))
+                        row.append(header.get("LONGITUD", "N/A"))
                     elif attribute == "ra":
-                        row.append(header.get('RA', 'N/A'))
+                        row.append(header.get("RA", "N/A"))
                     elif attribute == "dec":
-                        row.append(header.get('DEC', 'N/A'))
+                        row.append(header.get("DEC", "N/A"))
                     elif attribute == "exposure":
-                        row.append(header.get('EXPTIME', 'N/A'))
+                        row.append(header.get("EXPTIME", "N/A"))
                     # Add more attributes as needed
                     else:
                         row.append(f"Unknown attribute: {attribute}")
@@ -108,16 +110,38 @@ def display_header_info(fits_files, attributes):
 
 
 def add_argumnets_to_parser():
-    parser = argparse.ArgumentParser(description="Display information from FITS header and view FITS images.")
+    parser = argparse.ArgumentParser(
+        description="Display information from FITS header and view FITS images."
+    )
     parser.add_argument("fits_files", nargs="+", help="Paths to the FITS files")
-    parser.add_argument("-d", "--date", action="store_true", help="Display date from FITS header")
-    parser.add_argument("-t", "--time", action="store_true", help="Display time from FITS header")
-    parser.add_argument("-l", "--latitude", action="store_true", help="Display latitude from FITS header")
-    parser.add_argument("-o", "--longitude", action="store_true", help="Display longitude from FITS header")
+    parser.add_argument(
+        "-d", "--date", action="store_true", help="Display date from FITS header"
+    )
+    parser.add_argument(
+        "-t", "--time", action="store_true", help="Display time from FITS header"
+    )
+    parser.add_argument(
+        "-l",
+        "--latitude",
+        action="store_true",
+        help="Display latitude from FITS header",
+    )
+    parser.add_argument(
+        "-o",
+        "--longitude",
+        action="store_true",
+        help="Display longitude from FITS header",
+    )
     parser.add_argument("--ra", action="store_true", help="Display RA from FITS header")
-    parser.add_argument("--dec", action="store_true", help="Display DEC from FITS header")
-    parser.add_argument("--exposure", action="store_true", help="Display exposure time from FITS header")
-    parser.add_argument("-v", "--viewer", action="store_true", help="Display FITS file images")
+    parser.add_argument(
+        "--dec", action="store_true", help="Display DEC from FITS header"
+    )
+    parser.add_argument(
+        "--exposure", action="store_true", help="Display exposure time from FITS header"
+    )
+    parser.add_argument(
+        "-v", "--viewer", action="store_true", help="Display FITS file images"
+    )
     args = parser.parse_args()
     attributes = []
     if args.date:
@@ -138,9 +162,9 @@ def add_argumnets_to_parser():
     return parser
 
     attributes = []
-if __name__ == "__main__":
 
 
+def run_cli_script():
     parser = add_argumnets_to_parser()
     args = parser.parse_args()
     attributes = []
